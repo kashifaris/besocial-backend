2fb0eb48c192eebb8531f30eca4b01ca3d70b9ea
kashif khan <mokashifkhan21@gmail.com>
2023-03-19 20:36:34 +0530

    added basic file structure and setup the express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Error in running the server : ${err}`); //`is bacticks used for interpolation
+    }
+    console.log(`Server is running on the port : ${port}`);
+
+});
