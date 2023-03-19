b1dc55e4404e8003a7cd56e328942b302dd5b20c
kashif khan <mokashifkhan21@gmail.com>
2023-03-19 22:15:50 +0530

    added a controller and a router for home

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//use express router
+app.use('/',require('./routes')) //by default fetches index.js
+
 app.listen(port,(err)=>{
     if(err){
         console.log(`Error in running the server : ${err}`); //`is bacticks used for interpolation
